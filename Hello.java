20840bd55df50fc3777bccd49270ebb6c3a147e1
Hugo Felix Satriyawan <hugo_satriyawan_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 18:29:59 +0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello{
+    public static void main(String[] args) {
+        String nama = "Felix";
+        String alamat = "Surakarta";
+        int usia = 15;
+        char jenisKelamin = 'L';
+        double beratBadan = 103.00;
+
+        System.out.println("Nama:  "+nama);
+        System.out.println("Alamat: "+alamat);
+        System.out.println("Usia: "+usia);
+        System.out.println("Jenis Kelamin: "+jenisKelamin);
+        System.out.println("Berat Badan: "+beratBadan);
+    }
+}
